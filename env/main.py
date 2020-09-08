60d8fccf3d550b36b19e3159d31c152d52d2bea2
Xavier Fernandez <lain@MacBook-Pro.home>
2020-09-08 03:15:45 -0400

    dog and cat breeds added

--- env/main.py
+++ env/main.py
@@ -1,1 +1,25 @@
-hhh
+import os 
+import petpy 
+import pandas
+import json
+
+key = os.getenv('API_KEY')
+secret = os.getenv('SECRET')
+
+
+pf = petpy.Petfinder(key=key, secret=secret)
+
+def breeds(): 
+    cat_breeds = open("cats/cat_breeds.json","w+")
+    cats = pf.breeds('cat') 
+    cat_breeds.write(json.dumps(cats))
+    cat_breeds.close() 
+    dog_breeds = open("dogs/dog_breeds.json","w+")
+    dogs = pf.breeds('dog') 
+    dog_breeds.write(json.dumps(dogs))
+    dog_breeds.close() 
+    
+
+if __name__ == "__main__":
+    breeds()
+    pass
